a05f8085f8d5706721d76efe1a15bb27703457a6
AJNkamura <amanda.nkamura@gmail.com>
2024-03-16 01:07:16 -0300

    Compila mas da overflow

--- aula02/ex02/jackpot.c
+++ aula02/ex02/jackpot.c
@@ -2,24 +2,27 @@
 #include <time.h>
 
 void jackpot (int n, int r) {
-	int i;
+	int i, n0, n1, n2;
 	int size = 9;
 	int acertou = 0;
 	srand(time(NULL));
+	int n_voltas;
+	int flag_acertou = 0;
+	int elemento;
 
 //declara vetor com os 3 carreteis
 	Queue vetor_carreteis[3];
 
 //cria os 3 carreteis no vetor
 	for (i = 0; i < 3; i++){
-		vetor_carreteis[i] = create(size);
+		vetor_carreteis[i] =*create(size);
 	}
 	
 //preenche cada carretel
-	for (i = 0; i <= 9; i++){
-		enqueu (&vetor_carreteis[0], i);
-		enqueu (&vetor_carreteis[1], i);
-		enqueu (&vetor_carreteis[2], i);
+	for (i = 1; i <= size; i++){
+		enqueue (&vetor_carreteis[0], i);
+		enqueue (&vetor_carreteis[1], i);
+		enqueue (&vetor_carreteis[2], i);
 	}
 	while (!flag_acertou){
 //sorteia o numero de voltas que cada carrosel vai dar e roda os numeros
@@ -28,16 +31,16 @@ void jackpot (int n, int r) {
 			
 //enquanto numero de voltas nao for zero "gira" e move o primeiro numero para o final
 			while(n_voltas){
-				elemento = dequeue (&vetor_carretel[i]);
-				enqueue (&vetor_carretel[i], elemento);
+				elemento = dequeue (&vetor_carreteis[i]);
+				enqueue (&vetor_carreteis[i], elemento);
 				n_voltas --;
 			}
 		}
 	}
 	//na ultima vez que o carretel for mover, eu salvo o ultimo elemento removido de cada carretel para comparar e imprimir
-	n0 = dequeue (&vetor_carretel[0]);
-	n1 = dequeue (&vetor_carretel[1]);
-	n2 = dequeue (&vetor_carretel[2]);
+	n0 = dequeue (&vetor_carreteis[0]);
+	n1 = dequeue (&vetor_carreteis[1]);
+	n2 = dequeue (&vetor_carreteis[2]);
 	
 	//se acertou, muda a flag para nao continuar 
 	if (n0 == n1 && n1 == n2){
@@ -45,11 +48,11 @@ void jackpot (int n, int r) {
 
 	}	
 	//sempre q eu der dequeue tem q dar enqueue para nao ficar faltando elemento nas filas
-	enqueue(&vetor_carretel[0], n0);
-	enqueue(&vetor_carretel[1], n1);
-	enqueue(&vetor_carretel[2], n2);
+	enqueue(&vetor_carreteis[0], n0);
+	enqueue(&vetor_carreteis[1], n1);
+	enqueue(&vetor_carreteis[2], n2);
 
-	printf ("%d %d %d", n0 n1 n2);
+	printf ("%d %d %d", n0, n1, n2);
 
 }
 
@@ -59,5 +62,4 @@ int main () {
   int r = 10; /*sequência de números em cada carretel*/ 
   jackpot (n, r);
 }
-
 
