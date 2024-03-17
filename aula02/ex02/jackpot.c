96ec6024750a0ce35060a06be71527b402080c2a
AJNkamura <amanda.nkamura@gmail.com>
2024-03-17 01:39:37 -0300

    Correção e atualização final

--- aula02/ex02/jackpot.c
+++ aula02/ex02/jackpot.c
@@ -3,7 +3,7 @@
 
 void jackpot (int n, int r) {
 	int i, n0, n1, n2;
-	int size = 9;
+	int size =11;
 	int acertou = 0;
 	srand(time(NULL));
 	int n_voltas;
@@ -11,48 +11,50 @@ void jackpot (int n, int r) {
 	int elemento;
 
 //declara vetor com os 3 carreteis
-	Queue vetor_carreteis[3];
+	Queue* vetor_carreteis[3];
 
 //cria os 3 carreteis no vetor
 	for (i = 0; i < 3; i++){
-		vetor_carreteis[i] =*create(size);
+		vetor_carreteis[i] =create(size);
 	}
 	
 //preenche cada carretel
-	for (i = 1; i <= size; i++){
-		enqueue (&vetor_carreteis[0], i);
-		enqueue (&vetor_carreteis[1], i);
-		enqueue (&vetor_carreteis[2], i);
+	for (i = 1; i <= 9; i++){
+		enqueue (vetor_carreteis[0], i);
+		enqueue (vetor_carreteis[1], i);
+		enqueue (vetor_carreteis[2], i);
 	}
 	while (!flag_acertou){
 //sorteia o numero de voltas que cada carrosel vai dar e roda os numeros
 		for (i = 0; i < 3; i++){
 			n_voltas = rand() % 100;
-			
+						
 //enquanto numero de voltas nao for zero "gira" e move o primeiro numero para o final
 			while(n_voltas){
-				elemento = dequeue (&vetor_carreteis[i]);
-				enqueue (&vetor_carreteis[i], elemento);
+				elemento = dequeue (vetor_carreteis[i]);
+				enqueue (vetor_carreteis[i], elemento);
 				n_voltas --;
 			}
 		}
-	}
-	//na ultima vez que o carretel for mover, eu salvo o ultimo elemento removido de cada carretel para comparar e imprimir
-	n0 = dequeue (&vetor_carreteis[0]);
-	n1 = dequeue (&vetor_carreteis[1]);
-	n2 = dequeue (&vetor_carreteis[2]);
+
+//na ultima vez que o carretel for mover, eu salvo o ultimo elemento removido de cada carretel para comparar e imprimir
+	n0 = dequeue (vetor_carreteis[0]);
+	n1 = dequeue (vetor_carreteis[1]);
+	n2 = dequeue (vetor_carreteis[2]);
 	
+	printf ("%d %d %d \n", n0, n1, n2);
 	//se acertou, muda a flag para nao continuar 
 	if (n0 == n1 && n1 == n2){
 		flag_acertou = 1;
 
 	}	
 	//sempre q eu der dequeue tem q dar enqueue para nao ficar faltando elemento nas filas
-	enqueue(&vetor_carreteis[0], n0);
-	enqueue(&vetor_carreteis[1], n1);
-	enqueue(&vetor_carreteis[2], n2);
+	enqueue(vetor_carreteis[0], n0);
+	enqueue(vetor_carreteis[1], n1);
+	enqueue(vetor_carreteis[2], n2);
+	}
 
-	printf ("%d %d %d", n0, n1, n2);
+	
 
 }
 
