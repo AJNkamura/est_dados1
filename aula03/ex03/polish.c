efca5925fba95180d8e642577b74e16c8db3b1c4
AJNkamura <amanda.nkamura@gmail.com>
2024-03-24 16:18:36 -0300

    Ex03 finalizado

--- aula03/ex03/polish.c
+++ aula03/ex03/polish.c
@@ -5,35 +5,28 @@
 void reversed_polish_notation (char *c){
         Stack *s = create(strlen(c));
         Stack *aux = create(strlen(c));
-        Stack *polish = create(strlen(c));
         int i = 0;
         char elem;
         //enquanto nao chegar no elemento terminal
-        while (get_peek(s) != '\0' && !empty) {
-		//analisando o elemento do topo
-                elem = get_peek(s);
-		printf("%char", elem);
+        while (c[i] != '\0') {
+		elem = c[i];
                 //se o elemento em peek for um sinal, empilha ele na stack auxiliar
-                if (elem >= 42 && elem <= 47){
-		      	push (aux, elem);
+                if (elem >= '0' && elem <= '9'){
+		      	printf("%c", c[i]); 
                 }
-                //se for um numero, coloco na pilha
-                else if (elem >= '0' && elem <= '9'){
-			push (polish, elem);
+                //se for um abre, tira da pilha principal
+                else if (elem >= 42 && elem <= 47){
+			push(s, c[i]);
 		}
 		//se for um ')' ele tira o peek da pilha auxiliar e coloca na pilha polish
 		else if (elem == ')'){
-		//	pop (aux);
-			push (polish, elem);
+			printf ("%c", pop(s));
 		}
 	i++;
 	}
 
-	print(polish);
 	printf("\n");
 	destroy(s);
-	destroy(aux);
-	destroy(polish);
 	
 }
 
