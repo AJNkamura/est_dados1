8620cc5800c22051e59a488475ca1c4257aba486
Amanda Jury Nakamura <129013476+AJNkamura@users.noreply.github.com>
2024-04-01 13:27:14 -0300

    Add files lista05

---
+++ aula05/ex01/queue.c
@@ -0,0 +1,35 @@
+#include "queue.h"
+
+Queue* create () {
+  return NULL;
+}
+
+void destroy (Queue *q) {
+  while (q != NULL) {
+    /*Terminar*/
+  }
+}
+
+int empty (Queue *q) {
+  /*Terminar*/
+}
+
+Queue* enqueue (Queue *q, int elem) {
+  /*Terminar*/
+  /*Cria um elemento (malloc) e insere no final da lista.*/	
+}
+
+Queue* dequeue (Queue *q) {
+  /*Terminar*/
+  /*Remove o elemento (free) do início da lista.*/	
+}
+
+int get_front (Queue *q) {
+  /*Terminar*/
+  /*Retorna a informação (data) armazenada no início da lista.*/	
+}
+
+void print (Queue *q) {
+  /*Terminar*/
+}
+
