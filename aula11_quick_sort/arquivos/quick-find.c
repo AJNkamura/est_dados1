e94edb19d7fdc99dd227a181999da491dfd651a2
AJNkamura <amanda.nkamura@gmail.com>
2024-05-19 21:27:29 -0300

    Lista 11 exercicio 03

--- aula11_quick_sort/arquivos/quick-find.c
+++ aula11_quick_sort/arquivos/quick-find.c
@@ -1,9 +1,28 @@
 #include "utils.h"
 
-/* */
-void quick_find (int *A, int left, int right, int k) {
+int partition (int A[], int left, int right) {
+  int p = A[right];
+  int i = left - 1;
+  for (int j = left; j <= right-1; j++){
+    if (A[j] <= p){
+      i++;
+      swap(A, i, j);
+    }
+  }
+  swap(A, i+1, right);
+  return i+1;
+}
 
-
+void quick_find(int *A, int left, int right, int k) {
+  if (left <= right) {
+    int p = partition(A, left, right);
+    if (p == k)
+        printf("O elemento eh %d\n", A[p]);
+    else if (p > k)
+      quick_find(A, left, p - 1, k);
+    else
+      quick_find(A, p + 1, right, k);
+  }
 }
 
 /* */
